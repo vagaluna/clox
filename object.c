2e918ae900c1e6ad7de866c93408b132282b5dfe
Zhou Yi <kmyzzy@gmail.com>
2023-09-26 23:37:01 -0700

    26.2.2

--- object.c
+++ object.c
@@ -15,6 +15,11 @@ static Obj* allocateObject(size_t size, ObjType type) {
 	object->type = type;
 	object->next = vm.objects;
 	vm.objects = object;
+
+#ifdef DEBUG_LOG_GC
+  printf("%p allocate %zu for %d\n", (void*)object, size, type);
+#endif
+
 	return object;
 }
 
