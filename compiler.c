6dc9385e3e3d5e1ca60fef621cb4abb8e5a46fa1
Zhou Yi <kmyzzy@gmail.com>
2024-01-23 00:14:19 -0800

    28.4.2

--- compiler.c
+++ compiler.c
@@ -54,6 +54,7 @@ typedef struct {
 
 typedef enum {
 	TYPE_FUNCTION,
+	TYPE_INITIALIZER,
 	TYPE_METHOD,
 	TYPE_SCRIPT
 } FunctionType;
@@ -163,7 +164,12 @@ static int emitJump(uint8_t instruction) {
 }
 
 static void emitReturn() {
-	emitByte(OP_NIL);
+	if (current->type == TYPE_INITIALIZER) {
+    emitBytes(OP_GET_LOCAL, 0);
+  } else {
+    emitByte(OP_NIL);
+  }
+
 	emitByte(OP_RETURN);
 }
 
@@ -487,6 +493,11 @@ static void method() {
   uint8_t constant = identifierConstant(&parser.previous);
 
 	FunctionType type = TYPE_METHOD;
+	if (parser.previous.length == 4 &&
+      memcmp(parser.previous.start, "init", 4) == 0) {
+    type = TYPE_INITIALIZER;
+  }
+
   function(type);
   emitBytes(OP_METHOD, constant);
 }
@@ -595,6 +606,10 @@ static void returnStatement() {
   if (match(TOKEN_SEMICOLON)) {
     emitReturn();
   } else {
+		if (current->type == TYPE_INITIALIZER) {
+      error("Can't return a value from an initializer.");
+    }
+		
     expression();
     consume(TOKEN_SEMICOLON, "Expect ';' after return value.");
     emitByte(OP_RETURN);
@@ -758,7 +773,7 @@ static void this_(bool canAssign) {
     error("Can't use 'this' outside of a class.");
     return;
   }
-	
+
   variable(false);
 } 
 
