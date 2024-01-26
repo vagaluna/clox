17277c61479874bbded2dab069e3755f42933fb9
Zhou Yi <kmyzzy@gmail.com>
2024-01-26 00:01:38 -0800

    29.1.2

--- vm.c
+++ vm.c
@@ -175,7 +175,7 @@ static bool invoke(ObjString* name, int argCount) {
     vm.stackTop[-argCount - 1] = value;
     return callValue(value, argCount);
   }
-  
+
   return invokeFromClass(instance->klass, name, argCount);
 }
 
@@ -489,6 +489,18 @@ static InterpretResult run() {
           case OP_CLASS:
             push(OBJ_VAL(newClass(READ_STRING())));
             break;
+          case OP_INHERIT: {
+            Value superclass = peek(1);
+            if (!IS_CLASS(superclass)) {
+              runtimeError("Superclass must be a class.");
+              return INTERPRET_RUNTIME_ERROR;
+            }
+            ObjClass* subclass = AS_CLASS(peek(0));
+            tableAddAll(&AS_CLASS(superclass)->methods,
+                        &subclass->methods);
+            pop(); // Subclass.
+            break;
+          }
           case OP_METHOD:
             defineMethod(READ_STRING());
             break;
